a09c4ff71787531b23e47828cf5240e06bdacc06
Stephen Nacion <stephennacion30@gmail.com>
2022-07-19 04:40:22 +0800

    add pickle file

--- flask_app.py
+++ flask_app.py
@@ -9,9 +9,26 @@ with open('controls.pkl', 'wb') as f:
     pickle.dump(initial_control, f)
 
 
-@app.route('/')
-def helloHandler():
-    return render_template('remote_control.html')
+@app.route('/update')
+def updateHandler():
+    with open('controls.pkl', 'rb') as f:
+        controls_list = pickle.load(f)
+    
+    return str(controls_list)
+
+
+@app.route('/reset')
+def resetHandler():
+    with open('controls.pkl', 'rb') as f:
+        controls_list = pickle.load(f)
+    
+    controls_list[1] = 0
+    controls_list[0] = 0
+
+    with open('controls.pkl', 'wb') as f:
+        pickle.dump(controls_list, f)
+
+    return 'true'
 
 @app.route('/left_side')
 def left_side():
