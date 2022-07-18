7721223b3c7bc142bb467415f7d71d8265bbb6a1
Stephen Nacion <stephennacion30@gmail.com>
2022-07-19 04:28:19 +0800

    add pickle file

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 initial_control = [0,0]
 
-with open('controls.pkl', 'rb') as f:
-    mynewlist = pickle.load(f)
+with open('controls.pkl', 'wb') as f:
+    pickle.dump(initial_control, f)
 
 
 @app.route('/')
@@ -15,50 +15,73 @@ def helloHandler():
 
 @app.route('/left_side')
 def left_side():
-    print("left side")
+    with open('controls.pkl', 'rb') as f:
+        controls_list = pickle.load(f)
+    
+    controls_list[1] = 1
+
+    with open('controls.pkl', 'wb') as f:
+        pickle.dump(controls_list, f)
+
     return 'true'
 
 @app.route('/right_side')
 def right_side():
-    global speed, angle
-    data1 = "RIGHT"
-    angle += 1
-    if angle > 90:
-        angle = 90
-    print(speed, angle)
-    if serial_body_status == True:
-        send_serial_body(speed=speed, angle=angle)
+    with open('controls.pkl', 'rb') as f:
+        controls_list = pickle.load(f)
+    
+    controls_list[1] = 2
+
+    with open('controls.pkl', 'wb') as f:
+        pickle.dump(controls_list, f)
     return 'true'
 
 
 @app.route('/up_side')
 def up_side():
-    global speed, angle
-    data1 = "FORWARD"
-    speed = 4
-    print(speed, angle)
-    if serial_body_status == True:
-        send_serial_body(speed=speed, angle=angle)
+    with open('controls.pkl', 'rb') as f:
+        controls_list = pickle.load(f)
+    
+    controls_list[0] = 1
+
+    with open('controls.pkl', 'wb') as f:
+        pickle.dump(controls_list, f)
     return 'true'
 
 
 @app.route('/down_side')
 def down_side():
-    global speed, angle
-    speed = -4
-    print(speed, angle)
-    if serial_body_status == True:
-        send_serial_body(speed=speed, angle=angle)
+    with open('controls.pkl', 'rb') as f:
+        controls_list = pickle.load(f)
+    
+    controls_list[0] = 2
+
+    with open('controls.pkl', 'wb') as f:
+        pickle.dump(controls_list, f)
     return 'true'
 
 
 @app.route('/stop')
 def stop():
-    global speed, angle
-    speed = 0
-    print(speed, angle)
-    if serial_body_status == True:
-        send_serial_body(speed=speed, angle=angle)
+    with open('controls.pkl', 'rb') as f:
+        controls_list = pickle.load(f)
+    
+    controls_list[0] = 0
+
+    with open('controls.pkl', 'wb') as f:
+        pickle.dump(controls_list, f)
+    return 'true'
+
+@app.route('/straight')
+def straight():
+    print("straight")
+    with open('controls.pkl', 'rb') as f:
+        controls_list = pickle.load(f)
+    
+    controls_list[1] = 0
+
+    with open('controls.pkl', 'wb') as f:
+        pickle.dump(controls_list, f)
     return 'true'
  
 app.run(host='0.0.0.0', port= 8090)
